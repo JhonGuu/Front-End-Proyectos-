1a0ef7d93ff87d87124c3e1bb117455ed990edfc
Juan Jose Aranda <arandajuanjose17@gmail.com>
2024-01-13 13:06:43 -0300

    new

---
+++ pagina nueva/miApp/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+ return (
+    <>
+      <div className = "bg-orange-600 w-2/6 h-10">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
